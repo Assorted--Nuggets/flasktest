21a045b4366f25a7ee380261cfd82206721f721c
Max Dowling <mdow7299@student.liberty.k12.mo.us>
2015-10-14 09:35:47 -0400

    added bootstrap

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.socketio import SocketIO, emit, disconnect
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test'
 ws = SocketIO(app)
+bootstrap = Bootstrap(app)
 app.debug = True
 thread = None
 
-@app.route('/')
+class NickForm(Form):
+    name = StringField('Nickname: ', validators=[Required()])
+    submit = SubmitField('Set Nickname')
+
+@app.route('/chat')
+def chat():
+    if session.get('nick') != None:
+        return render_template('index.html', nickname=session.get('nick'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if session.get('nick') != None:
+        return redirect(url_for('chat'))
+    form = NickForm()
+    
+    if form.validate_on_submit():
+        session['nick'] = form.name.data
+        form.name.data = ""
+        return redirect(url_for('chat'))
+    
+    return render_template('nick.html', form=form)
 
 @ws.on('message sent')
 def message_sent(message):
     emit('broadcast', message, broadcast=True)
+
 if __name__ == '__main__':
     ws.run(app)
